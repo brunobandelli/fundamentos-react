8d2dc88b7edc26c998dce9bdb06d1c8402ef9df1
brunobandelli <brunobandelli@gmail.com>
2021-09-10 23:00:30 -0300

    Criando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import './App.css'
 
+import Card from './components/layout/Card'
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -7,16 +9,31 @@ import Fragmento from './components/basicos/Fragmento'
 
 export default _ => 
 
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60} />
-            <Fragmento />
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Bruno" 
-                nota={10}  
-            />
-            <Primeiro></Primeiro>
+
+            <div className="Cards">
+                <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+                </Card>
+
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+                </Card>
+
+                <Card titulo="#02 - Com Parâmetro" color="E8B71A">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Bruno" 
+                    nota={10}  
+                />
+                </Card>
+
+                <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+                </Card>
+
+            </div>
         </div>
 
 
