7843999694146bb720ec4b4e5be4eed46fbd8581
brunobandelli <brunobandelli@gmail.com>
2021-09-21 20:46:59 -0300

    CONTADOR

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css'
 
+import Contador from "./components/contador/Contador"
 import Input from "./components/formulario/Input";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import DiretaPai from "./components/comunicacao/DiretaPai";
@@ -22,6 +23,10 @@ export default _ =>
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+                <Card titulo="#12 - Contador" color="#424242">
+                        <Contador numeroInicial={10}></Contador>
+                </Card>
+
                 <Card titulo="#11 - Componente Controlado" color="#E45F56">
                         <Input></Input>
                 </Card>
--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -28,13 +28,24 @@ class Contador extends Component {
 
     render() {
         return (
-            <div>
+            <div className="Contador">
                 <h2>Contador</h2>
                 <h3>{this.state.numero}</h3>
+            <div>
+                <label htmlFor="passoInput">Passo: </label>
+                <input 
+                    id="passoInput"
+                    type="number" 
+                    value={this.state.passo}
+                    onChange={this.setPasso}
+                />
             </div>
-            
-
+                <button onClick={this.inc}>+</button>
+                <button onClick={this.dec}>-</button>
+            </div>
 
         )
     }
 }
+
+export default Contador
