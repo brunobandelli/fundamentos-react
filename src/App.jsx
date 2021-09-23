09f63916e85ed8d57bba29e08318ed46a74731f4
brunobandelli <brunobandelli@gmail.com>
2021-09-22 21:35:39 -0300

    Desafio Mega Sena

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css'
 
+import Mega from "./components/mega/Mega";
 import Contador from "./components/contador/Contador"
 import Input from "./components/formulario/Input";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
@@ -23,6 +24,10 @@ export default _ =>
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+                <Card titulo="#13 - Mega" color="#B9006E">
+                        <Mega qtde={8}></Mega>
+                </Card>
+
                 <Card titulo="#12 - Contador" color="#424242">
                         <Contador numeroInicial={10}></Contador>
                 </Card>
